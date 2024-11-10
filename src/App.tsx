88c9d8c0d0b2704d1efb4ef0aa67cc374e2ddfc0
sercanio <104576153+sercanio@users.noreply.github.com>
2024-11-10 18:34:57 +0300

    feat: update layout width

--- src/App.tsx
+++ src/App.tsx
@@ -31,28 +31,30 @@ function App() {
       <ConfigProvider theme={{
         algorithm: isDarkMode ? theme.darkAlgorithm : theme.defaultAlgorithm,
       }}>
-        <Layout style={{ minHeight: '100vh' }}>
-          <Sidebar collapsed={collapsed} />
-          <Layout>
-            <Header 
-              collapsed={collapsed}
-              setCollapsed={setCollapsed}
-              isDarkMode={isDarkMode}
-              setDarkMode={setIsDarkMode}
-            />
-            <Content 
-              style={{ 
-                margin: '24px 16px', 
-                padding: 24,
-                background: isDarkMode ? '#141414' : '#fff',
-                minHeight: 280
-              }}
-            >
-              <AppRoutes />
-            </Content>
-            <Footer />
+        <div style={{ minHeight: '100vh', background: isDarkMode ? '#141414' : '#fff' }}>
+          <Layout style={{ minHeight: '100vh', maxWidth: 1920, margin: '0 auto' }}>
+            <Sidebar collapsed={collapsed} />
+            <Layout>
+              <Header
+                collapsed={collapsed}
+                setCollapsed={setCollapsed}
+                isDarkMode={isDarkMode}
+                setDarkMode={setIsDarkMode}
+              />
+              <Content
+                style={{
+                  margin: '24px 16px',
+                  padding: 24,
+                  background: isDarkMode ? '#141414' : '#fff',
+                  minHeight: 280
+                }}
+              >
+                <AppRoutes />
+              </Content>
+              <Footer />
+            </Layout>
           </Layout>
-        </Layout>
+        </div>
       </ConfigProvider>
     </BrowserRouter>
   );
