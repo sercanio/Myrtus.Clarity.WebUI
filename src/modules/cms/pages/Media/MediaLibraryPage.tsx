3ed03e79b6f597b8bd24a64d42e877e7194037e8
Sercan Ateş <104576153+sercanio@users.noreply.github.com>
2025-02-09 10:43:52 +0300

    refactor: remove unused components and utility functions from CMS module

--- src/modules/cms/pages/Media/MediaLibraryPage.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import MediaLibrary from '@src/modules/cms/components/Media/MediaLibrary';
-
-const MediaLibraryPage: React.FC = () => <MediaLibrary />;
-
-export default MediaLibraryPage;
